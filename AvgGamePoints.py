f82142302e313f66c6a25f20a2f229a0e37fc7b2
jduffymclaughlin <jduffymclaughlin@gmail.com>
2018-02-11 19:44:39 -0500

    ran with more sims

--- AvgGamePoints.py
+++ AvgGamePoints.py
@@ -1,18 +1,20 @@
 from CardDeck import PitchDeck
 import pandas as pd
+from tqdm import tqdm
 import matplotlib.pyplot as plt
+
 
 gameResults = []
 
+for i in tqdm(range(10000000)):
 
-for i in range(10):
-
-    deck = PitchDeck(printing=True)
+    deck = PitchDeck(printing=False)
 
     deck.shuffle()
     deck.deal()
     deck.bid()
     deck.exchange()
+
     hands = deck.finalHands
 
     gamePointsInPlay = 0
@@ -21,6 +23,7 @@ for i in range(10):
             gamePointsInPlay += card.gamePoints
     gameResults.append(gamePointsInPlay)
     #print(gamePointsInPlay)
+
 df = pd.DataFrame(gameResults, columns=['GamePointsInPlay'])
 print(df.describe())
 
