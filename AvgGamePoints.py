6d265923d2a3c3bc7fc12e4cbdab7279b5d92316
jduffymclaughlin <jduffymclaughlin@gmail.com>
2018-03-07 19:31:56 -0500

    parallelized simulations

--- AvgGamePoints.py
+++ AvgGamePoints.py
@@ -2,30 +2,47 @@ from CardDeck import PitchDeck
 import pandas as pd
 from tqdm import tqdm
 import matplotlib.pyplot as plt
-from tqdm import tqdm
+import time
+from multiprocessing import Pool
+
+start = time.time()
+cards_seen = []
+num_sims = 100000000
+chunks = [(0, num_sims // 4), 
+          (1 + num_sims // 4, num_sims // 2), 
+          (1 + num_sims // 2, 3 * num_sims // 4), 
+          (1 + 3 * num_sims // 4, num_sims-1)]
+
+game_results = list()
+
+def sim(chunk):
+    start, end = chunk
+    game = list()
+
+    for _ in range(start, end + 1):
+
+        deck = PitchDeck(printing=False)
+        deck.shuffle()
+        deck.deal()
+        deck.bid()
+        deck.exchange()
+        hands = deck.finalHands
+
+        gamePointsInPlay = 0
+        for player, hand in hands.items():
+            for card in hand:
+                gamePointsInPlay += card.gamePoints
+        game.append(gamePointsInPlay)
+    return game
+
+with Pool(4) as p:
+    game_results = p.map(sim, chunks)
+
+game_results = [i for o in game_results for i in o]
+print("TOTAL TIME {}".format(time.time() - start))
 
 
-gameResults = []
-
-for _ in tqdm(range(1000000)):
-
-    deck = PitchDeck(printing=False)
-
-    deck.shuffle()
-    deck.deal()
-    deck.bid()
-    deck.exchange()
-
-    hands = deck.finalHands
-
-    gamePointsInPlay = 0
-    for player, hand in hands.items():
-        for card in hand:
-            gamePointsInPlay += card.gamePoints
-    gameResults.append(gamePointsInPlay)
-    #print(gamePointsInPlay)
-
-df = pd.DataFrame(gameResults, columns=['GamePointsInPlay'])
+df = pd.DataFrame(game_results, columns=['GamePointsInPlay'])
 print(df.describe())
 
 df.plot(kind='hist', bins=30)
--- CardsSeen.py
+++ CardsSeen.py
@@ -2,23 +2,43 @@ from CardDeck import PitchDeck
 import pandas as pd
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+from multiprocessing import Pool
+import numpy as np
+import time
 
+start = time.time()
 cards_seen = []
+num_sims = 1000000
+chunks = [(0, num_sims // 4), 
+          (1 + num_sims // 4, num_sims // 2), 
+          (1 + num_sims // 2, 3 * num_sims // 4), 
+          (1 + 3 * num_sims // 4, num_sims)]
 
-for _ in tqdm(range(1000000)):
-    deck = PitchDeck(printing=False)
+def sim(chunk):
+    start, end = chunk
 
-    deck.shuffle()
-    deck.deal()
-    deck.bid()
-    deck.exchange()
+    results = list()
+    for _ in range(start, end + 1):
+        deck = PitchDeck(printing=False)
 
-    cards_seen.append(52 - len(deck.deck))
+        deck.shuffle()
+        deck.deal()
+        deck.bid()
+        deck.exchange()
 
+        results.append(52 - len(deck.deck))
+    print(results)
+    return results
+
+
+with Pool(4) as p:
+    cards_seen = p.map(sim, chunks)
+
+cards_seen = [s for c in cards_seen for s in c]
+print("TOTAL TIME {}".format(time.time() - start))
 
 df = pd.DataFrame(cards_seen, columns=['CardsSeen'])
 print(df.describe())
 df.plot(kind='hist', bins=5)
 plt.show()
-
 
